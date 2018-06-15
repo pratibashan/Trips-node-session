b75f723b6ef078660084c49d131cf944b2f4c0e6
Pratiba Shan <pratibashan.g@gmail.com>
2018-06-14 22:36:04 -0500

    Trips Assignment with session management

---
+++ app.js
@@ -0,0 +1,167 @@
+
+const express = require('express')
+const mustacheExpress = require('mustache-express')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+
+const app = express()
+
+let currentUser = {} 
+
+//init session
+app.use(session({
+    secret: 'TripsInfo',
+    resave: false,
+    saveUninitialized: false
+  }))
+
+let tripsArray = []
+let userArray = []
+
+app.engine('mustache',mustacheExpress())
+app.set('views','./views')
+app.set('view engine','mustache')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(express.static('public'))
+
+
+
+app.get('/register',function(req,res){
+    res.render('register')
+})
+
+
+app.post('/register',function(req,res){
+
+
+    let username = req.body.userName.toLowerCase()
+    let userpassword = req.body.userPassword.toLowerCase()
+
+    let user = { name : username , password : userpassword, trips : []}
+
+    userArray.push(user)
+
+    //res.send('registered')
+    res.redirect('login')
+})   
+
+app.get('/login',function(req,res){
+    res.render('login')
+})
+
+
+app.post('/login',function(req,res){
+
+    
+    let loginName = req.body.loginName.toLowerCase()
+    let loginPassword = req.body.loginPassword.toLowerCase()
+
+     // get the user out of the array 
+     // matching username and password 
+    currentUser = userArray.find(function(user){
+        return user.name == loginName && user.password == loginPassword
+    })
+
+    console.log(currentUser)
+
+    if(currentUser) {
+        if(req.session) { 
+            req.session.username = loginName  
+            //setting the expiration date for the cookie    
+            var hour = 3600000
+            req.session.cookie.expires = new Date(Date.now() + hour)
+            req.session.cookie.maxAge = hour
+            }
+        res.redirect('admin/index')    
+    }
+    else {
+        
+        res.redirect('register')
+       
+    }   
+  
+
+})   
+
+function validateLogin(req,res,next) {
+
+        if(req.session.username) {
+          next()
+        } else {
+          res.redirect('/login')
+        }
+      
+      }
+app.all('/admin/*',validateLogin,function(req,res,next){
+        next()
+    })
+          
+ 
+app.get('/admin/index',function(req,res){
+
+     res.render('admin/index',{username : req.session.username})    
+})
+
+
+app.post('/admin/trips',function(req,res){
+
+    console.log(req.body.tripImageUrl)
+    let tripName = req.body.tripName
+    let tripImageUrl = req.body.tripImageUrl
+    let departureDate = req.body.dateDepature
+    let returnDate = req.body.dateReturn
+                
+    if(req.session) {
+
+        currentUser.trips.push({
+            tripId : guid(),
+            tripName : tripName,
+            tripImageUrl:tripImageUrl,
+            departureDate: departureDate,
+            returnDate: returnDate
+        })
+        
+    }            
+  
+    
+    res.render('admin/trips',{tripList : currentUser.trips})
+  
+})
+
+app.get("/admin/trips",function(req,res){
+    res.render('admin/trips',{tripList : currentUser.trips})
+})
+
+app.post('/deleteTrip',function(req,res){
+    let tripId = req.body.tripId
+
+    let trips = currentUser.trips 
+
+    currentUser.trips = trips.filter(function(trip){
+        return trip.tripId != tripId
+    })
+    
+    res.redirect('admin/trips')
+})
+
+app.post('/logout',function(req,res){
+    req.session.destroy()
+    currentUser={}
+    res.redirect('/login')
+})
+
+
+function guid() {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+        }
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+      }
+
+app.listen(3000,function(){
+    console.log('app listening on port 3000')
+})
